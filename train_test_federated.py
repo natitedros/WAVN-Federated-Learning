0331459fd0a807d7876478f84f7d95a28965f542
natitedros <tedros.natnael@gmail.com>
2025-11-18 01:16:41 -0500

    feat: added fl python code

--- train_test_federated.py
+++ train_test_federated.py
@@ -1,28 +1,14 @@
-import os
 import torch
-import numpy as np
-import logging.config
-from tqdm import tqdm
-from model import CNN
-import matplotlib.pyplot as plt
 import copy
+from tqdm import tqdm
 
-NUM_EPOCHS = 10
-LOCAL_ITERS = 2
-VIS_DATA = False
-BATCH_SIZE = 5
-NUM_CLIENTS = 2
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print('Device:', DEVICE)
 
-# DATASET = "fashion_mnist"
-DATASET = "mnist"
-
-def FedAvg(params):
+def federated_average(params):
     """
-    Average the paramters from each client to update the global model
-    :param params: list of paramters from each client's model
-    :return global_params: average of paramters from each client
+    Average the parameters from each client to update the global model.
+    
+    :param params: list of parameters from each client's model
+    :return: averaged parameters from all clients
     """
     global_params = copy.deepcopy(params[0])
     for key in global_params.keys():
@@ -32,116 +18,124 @@ def FedAvg(params):
     return global_params
 
 
-def train(local_model, device, dataset, iters):
+def train_local_model(model, device, dataloader, num_iterations, lr=0.001):
     """
-    Trains a local model for a given client
-    :param local_model: a copy of global CNN model required for training
-    :param device: the device used to train the model - GPU/CPU
-    :param dataset: training dataset used to train the model
-    :return local_params: parameters from the trained model from the client
-    :return train_loss: training loss for the current epoch
-    """    
-    #optimzer for training the local models
-    local_model.to(device)
+    Train a local model for a given client.
+    
+    :param model: local model (copy of global model)
+    :param device: device to train on (GPU/CPU)
+    :param dataloader: training data for this client
+    :param num_iterations: number of local training iterations
+    :param lr: learning rate
+    :return: (model parameters, average training loss)
+    """
+    model.to(device)
     criterion = torch.nn.CrossEntropyLoss().to(device)
-    optimizer = torch.optim.Adam(local_model.parameters(), lr=0.001)
-    train_loss = 0.0
-    local_model.train()
-    #Iterate for the given number of Client Iterations
-    for i in range(iters):
-        batch_loss = 0.0
-        for batch_idx, (data, target) in tqdm(enumerate(dataset), total=len(dataset)):
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    
+    model.train()
+    total_loss = 0.0
+    
+    for iteration in range(num_iterations):
+        epoch_loss = 0.0
+        for batch_idx, (data, target) in enumerate(dataloader):
             data, target = data.to(device), target.to(device)
-            #set gradients to zero            
+            
             optimizer.zero_grad()
-            #Get output prediction from the Client model
-            output = local_model(data)
-            #Computer loss
+            output = model(data)
             loss = criterion(output, target)
-            batch_loss += loss.item()*data.size(0)
-            #Collect new set of gradients
+            epoch_loss += loss.item() * data.size(0)
             loss.backward()
-            #Update local model
             optimizer.step()
-        #add loss for each iteration
-        train_loss+=batch_loss/len(dataset)
-    return local_model.state_dict(), train_loss/iters
+        
+        total_loss += epoch_loss / len(dataloader.dataset)
+    
+    avg_loss = total_loss / num_iterations
+    return model.state_dict(), avg_loss
 
 
-def test(model, dataloader):
+def federated_learning(global_model, client_dataloaders, device, 
+                       num_epochs, local_iterations, lr=0.001):
     """
-    Tests the Federated global model for the given dataset
-    :param model: Trained CNN model for testing
-    :param dataloader: data iterator used to test the model
-    :return test_loss: test loss for the given dataset
-    :return preds: predictions for the given dataset
-    :return accuracy: accuracy for the prediction values from the model
-    """    
-    criterion = torch.nn.CrossEntropyLoss()
-    test_loss = 0.0
-    correct = 0
-    model.eval()
-    for batch_idx, (data, target) in tqdm(enumerate(dataloader), total=len(dataloader.dataset)/BATCH_SIZE):
-        data, target = data, target
-        output = model(data)
-        loss = criterion(output, target)
-        test_loss += loss.item()*data.size(0)
-        preds = output.argmax(dim=1, keepdim=True)
-        correct += preds.eq(target.view_as(preds)).sum().item()
-    accuracy = correct / len(dataloader.dataset)
+    Run federated learning across multiple clients.
     
-    return test_loss/len(dataloader.dataset), preds, accuracy
-
-
-if __name__=="__main__":
-    if not os.path.isdir('models'):
-        os.mkdir('models')
-    if not os.path.isdir('results'):
-        os.mkdir('results')
+    :param global_model: initial global model
+    :param client_dataloaders: list of dataloaders, one per client
+    :param device: device to train on (GPU/CPU)
+    :param num_epochs: number of federated learning rounds
+    :param local_iterations: number of local training iterations per round
+    :param lr: learning rate
+    :return: (trained global model, list of training losses per epoch)
+    """
+    num_clients = len(client_dataloaders)
+    all_train_losses = []
     
-    #Initialize a logger to log epoch results
-    logname = ('results/log_federated_' + DATASET + "_" + str(NUM_EPOCHS) +"_"+ str(NUM_CLIENTS) + "_" + str(LOCAL_ITERS))
-    logging.basicConfig(filename=logname,level=logging.DEBUG)
-    logger = logging.getLogger()
-    
-    
-    #get data
-    # train_data, validation_data, test_data = load_dataset(val_split=0.2, batch_size=BATCH_SIZE, dataset=DATASET)
-
-    #distribute the trainning data across clients
-    train_distributed_dataset = [[]]
-    
-    #get Nasim's model and define criterion for loss
-    global_model = CNN()
-    global_params = global_model.state_dict()
-    
-    global_model.train()
-    all_train_loss = list()
-    all_val_loss = list()
-    val_loss_min = np.Inf
-    
-    #Train the model for given number of epochs
-    for epoch in range(1, NUM_EPOCHS+1):
-        print("\nEpoch :", str(epoch))
-        local_params, local_losses = [], []
-        #Send a copy of global model to each client
-        for idx in range(NUM_CLIENTS):
-            #Perform training on client side and get the parameters
-            param, loss = train(copy.deepcopy(global_model), DEVICE, train_distributed_dataset[idx],LOCAL_ITERS)
-            local_params.append(copy.deepcopy(param))
-            local_losses.append(copy.deepcopy(loss))
+    for epoch in range(1, num_epochs + 1):
+        print(f"\nFederated Learning Round: {epoch}/{num_epochs}")
         
-        #Federated Average for the paramters from each client
-        global_params = FedAvg(local_params)
-        #Update the global model
+        local_params = []
+        local_losses = []
+        
+        # Train on each client
+        for client_id in range(num_clients):
+            print(f"  Training client {client_id + 1}/{num_clients}")
+            
+            # Create a copy of global model for this client
+            local_model = copy.deepcopy(global_model)
+            
+            # Train locally
+            params, loss = train_local_model(
+                local_model, 
+                device, 
+                client_dataloaders[client_id],
+                local_iterations,
+                lr
+            )
+            
+            local_params.append(params)
+            local_losses.append(loss)
+        
+        # Aggregate parameters using federated averaging
+        global_params = federated_average(local_params)
         global_model.load_state_dict(global_params)
-        all_train_loss.append(sum(local_losses)/len(local_losses))
         
-        #Test the global model
-        
-        #if validation loss decreases, save the model
-        
-    #load the best model from training
+        # Record average loss across clients
+        avg_loss = sum(local_losses) / len(local_losses)
+        all_train_losses.append(avg_loss)
+        print(f"  Average training loss: {avg_loss:.4f}")
     
-    #test the model using test data
+    return global_model, all_train_losses
+
+
+# Example usage:
+if __name__ == "__main__":
+    """
+    Example of how to use the federated learning framework:
     
+    # 1. Define your model
+    model = MyCNNModel()
+    
+    # 2. Prepare client dataloaders
+    client_dataloaders = [
+        DataLoader(client1_dataset, batch_size=32, shuffle=True),
+        DataLoader(client2_dataset, batch_size=32, shuffle=True),
+        # ... more clients
+    ]
+    
+    # 3. Set device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    # 4. Run federated learning
+    trained_model, losses = federated_learning(
+        global_model=model,
+        client_dataloaders=client_dataloaders,
+        device=device,
+        num_epochs=10,
+        local_iterations=2,
+        lr=0.001
+    )
+    
+    # 5. Evaluate or save your model
+    torch.save(trained_model.state_dict(), 'federated_model.pth')
+    """
+    pass
